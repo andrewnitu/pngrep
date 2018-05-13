f0dcf1aa3b17f9621471eb371b8f2ab7822d4205
Andrew Nitu <andrewnitu@gmail.com>
2018-05-13 01:40:49 -0400

    Change OCR to read directly input filename string

--- src/ocr/ocr.py
+++ src/ocr/ocr.py
@@ -2,5 +2,5 @@ from PIL import Image
 import pytesseract
 
 
-def get_string(file):
-    return pytesseract.image_to_string(Image.open(file.full_path))
+def get_text(file):
+    return pytesseract.image_to_string(Image.open(file))
