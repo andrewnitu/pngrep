b4fd21cb8b76ae63a4995d13ca3efbef6daa77b5
Andrew Nitu <andrewnitu@gmail.com>
2018-05-13 01:42:17 -0400

    Add updated_date field to FileText model for incremental updates

--- src/db/model/file_text.py
+++ src/db/model/file_text.py
@@ -6,6 +6,7 @@ database = peewee.SqliteDatabase("storage.db")
 class FileText(peewee.Model):
     path = peewee.CharField()
     text = peewee.CharField()
+    updated_date = peewee.DateTimeField()
 
     class Meta:
         database = database
