8e51b78c388794b7fe145f9442539a952d885d8f
Andrew Nitu <andrewnitu@gmail.com>
2018-06-09 01:55:48 -0400

    Improve code

--- src/files/lookup.py
+++ src/files/lookup.py
@@ -4,6 +4,8 @@ from datetime import datetime
 from termcolor import colored
 import db.db as db
 
+FILENAME_COLOR = 'green'
+
 
 def __absolute_path_string(path):
     return str(path.absolute())
@@ -13,19 +15,21 @@ def __file_name_string(path):
     return path.name
 
 
+# Returns a formatted and colored string of results, including the filename of each result
 def lookup_no_cache(string, filenames):
     files = map(lambda x: Path(x), filenames)
 
     answer = ""
 
     for file in files:
-        answer += colored(file.name + ": ", 'green')
+        answer += colored(file.name + ": ", FILENAME_COLOR)
         answer += db.read_file_text(__absolute_path_string(file)).text
         answer += "\n"
 
     return answer
 
 
+# Returns a formatted and colored string of results, including the filename of each result
 def lookup_with_cache(string, filenames):
     files = map(lambda x: Path(x), filenames)
 
@@ -38,7 +42,7 @@ def lookup_with_cache(string, filenames):
         # if there's no cache entry
         if not db.exists_file_text(absolute_path_string):
             current_text = ocr.get_text(absolute_path_string)
-            answer += colored(file_name_string + ": ", 'green')
+            answer += colored(file_name_string + ": ", FILENAME_COLOR)
             answer += ocr.get_text(absolute_path_string)
             answer += "\n"
             db.save_file_text(absolute_path_string, current_text)
@@ -46,14 +50,14 @@ def lookup_with_cache(string, filenames):
         elif db.read_file_text(absolute_path_string).updated_date < \
                 datetime.fromtimestamp(file.stat().st_mtime):
             current_text = ocr.get_text(absolute_path_string)
-            answer += colored(file_name_string + ": ", 'green')
+            answer += colored(file_name_string + ": ", FILENAME_COLOR)
             answer += ocr.get_text(absolute_path_string)
             answer += "\n"
             db.clear_file_text(absolute_path_string)
             db.save_file_text(absolute_path_string, current_text)
         # if the cache entry is current
         else:
-            answer += colored(file_name_string + ": ", 'green')
+            answer += colored(file_name_string + ": ", FILENAME_COLOR)
             answer += db.read_file_text(absolute_path_string).text
             answer += "\n"
 
