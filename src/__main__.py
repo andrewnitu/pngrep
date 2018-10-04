0eabefb2cb6dbd4f98818220240e05db1115621f
Andrew Nitu <andrewnitu@gmail.com>
2018-10-04 01:51:11 -0400

    add parallel parameter to main

--- src/__main__.py
+++ src/__main__.py
@@ -16,15 +16,16 @@ def cli():
 # Search and update
 @cli.command()
 @click.option('--no-cached', '-n', is_flag=True)
+@click.option('--parallel', '-p', is_flag=True)
 @click.argument('search-string', required=True)
 @click.argument('files', nargs=-1, type=click.Path('rb'))
-def search(no_cached, search_string, files):
+def search(no_cached, parallel, search_string, files):
     db.init()
 
     if no_cached:
-        sys.stdout.write(formatting.format_results(lookup.lookup_no_cache(files), search_string))
+        sys.stdout.write(formatting.format_results(lookup.lookup_no_cache(files, parallel), search_string))
     else:
-        sys.stdout.write(formatting.format_results(lookup.lookup_with_cache(files), search_string))
+        sys.stdout.write(formatting.format_results(lookup.lookup_with_cache(files, parallel), search_string))
 
 
 # Clear the cache
