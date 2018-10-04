9782365339ae0ff08b9bc1723ac41799f9e16f8c
Andrew Nitu <andrewnitu@gmail.com>
2018-10-04 01:51:00 -0400

    Add multiprocessiing

---
+++ src/processing/processing_params.py
@@ -0,0 +1,5 @@
+class ProcessingParams:
+    def __init__(self, index, absolute_path_string, reason):
+        self.index = index
+        self.absolute_path_string = absolute_path_string
+        self.reason = reason
